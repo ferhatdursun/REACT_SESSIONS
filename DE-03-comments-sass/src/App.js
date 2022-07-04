4a2ea25978163913244d16f08ccc436b815415e0
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-07-04 11:59:03 +0200

    JS

--- DE-03-comments-sass/src/App.js
+++ DE-03-comments-sass/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import "./App.scss"
+import "./App.scss";
+import Card from './components/card/Card';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Card from "./components/card/Card";
-import datA from "./data";
+import datA from "./data"
+
 
 const App = () => {
   return (
     <div>
-    <Header/>
-    <Card  />
+     <Header/>
+    <Card dataa={datA} />
+    <Footer/>
     </div>
   );
 }
