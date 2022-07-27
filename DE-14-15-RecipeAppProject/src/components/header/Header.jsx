b26f7b79dd3a7635278d429023d47643a8beadad
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-07-27 06:21:13 +0200

    React

--- DE-14-15-RecipeAppProject/src/components/header/Header.jsx
+++ DE-14-15-RecipeAppProject/src/components/header/Header.jsx
@@ -9,26 +9,34 @@ import {
 } from "./HeaderStyles";
 import homeFoto from "../../assets/home.svg";
 
-const Header = () => {
-  return (
-    <div>
-      <HeaderContainer>
-        <MainHeader>Food App</MainHeader>
+//! Home sayfasinda return kismindan gönderilenleri burada Header'in icerisinde süslü ile karsilamamiz gerekiyor.
+const Header = ({ setQuery, setMeal, getData }) => {
+  const submit = () => {
+    e.preventDefault(); //! preventDefault yazmazsak gelen verileri direkt olarak Backend'e gönderiyor. Bunu engellemek icin preventDefault yazdik.
+    getData(); //! getData Home sayfasinda tanimli ve oradan geliyor.
+  };
 
-        <FormContainer>
-          <FoodInput placeholder="Search"></FoodInput>
-          <Button>Search</Button>
-          <Select>
-            <option value="breakfest">Breakfest</option>
-            <option value="lunch">Lunch</option>
-            <option value="dinner">Dinner</option>
-            <option value="snack">Snack</option>
-            <option value="teatime">Teatime</option>
-          </Select>
-        </FormContainer>
-        <img src={homeFoto} alt="" width="50%" className="image" />
-      </HeaderContainer>
-    </div>
+  return (
+    <HeaderContainer>
+      <MainHeader>FOOD APP</MainHeader>
+      <FormContainer onSubmit={submit}>
+        <FoodInput
+          type={text}
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+          //! onChange;  herhangi bir veri girisi olursa eger bunu e olarak algila ve setQuerynin icerisine at.
+        />
+        <Button type="submit">Search</Button>
+        <Select
+          name="mealTypes"
+          id="mealTypes"
+          onChange={(e) => setMeal(e.target.value)}
+        >
+          <option>Breakfast</option>
+          <option>Lunch</option>
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
   );
 };
 
--- DE-14-15-RecipeAppProject/src/pages/home/Home.jsx
+++ DE-14-15-RecipeAppProject/src/pages/home/Home.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
-import  Header  from "../../components/header/Header";
+import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import axios from "axios";
+
 const APP_ID = "ebee76fe";
 const APP_KEY = "5cc1ea7173fe56f0a274fcß6e7818a55";
 /****buraya kendi id ve key imizi yaziyoruz**********/
 
 const Home = () => {
   const [query, setQuery] = useState("");
-
+  //! url icerisinde ${query}, input icerisine girilen yemek adina göre arama yapiyor. Bunun icin de queryi burada useState ile tanimladik.
+  const [yemekler, setYemekler] = useState([]);
   const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
+  //! meal de ayni query gibi ayni sebepten dolayi burada useState ile tanimlandi. Ve mealTypes[1] olursa inpu ici defaul olarak 1. index veya 2. 3. index numarali eleman görünür.
 
   // query=yazdığımız sorgu kelimesi, meal=breakfast vs
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
 
+  //! URL den veri cekme islemi
+
+  const getData = async () => {
+    const veri = await axios.get(url);
+    setYemekler(veri.data.hits);
+    //! Veriler axios ile cekilirse json yapmaya gerek kalmiyor.
+    //! sonrasinda bu verileri icerisine atmak icin bir useState ihtiyaci doguyor. yemekler
+    //! setYemekler(veri) yapinca veri adinda ki urlden gelen datalar suanda yemeklerin icerisine gömüldü.
+  };
+
   return (
     <div>
-      
-      <Header/>
+      {/* Veriler url ile Home sayfasinda cekildiginden ve useStatelerin degistirebilme yetenekleri ile bu veriler üzerinde oynamalar yapabildigimizden dolayi bunlarin hepsini burada Header sayfasina göndermemmiz gerekiyor. */}
+      {/*  Buradaki gönderilenlerden dolayi Header sayfasina ne yazilirsa ekrana bastirilir. */}
+      <Header setQuery={setQuery} setMeal={setMeal} getData={getData} />
     </div>
-  )
+  );
 };
 
 export default Home;
